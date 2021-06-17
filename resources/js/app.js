2971a978c9f358f24d53c5c24d7de24891c4f7de
feehm27 <feehmfontoura@hotmail.com>
2021-06-16 23:22:34 -0300

    Integração do PHP com React

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,4 @@
-require('./bootstrap');
+import Teste from "./components/Teste";
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<Teste />, document.getElementById('teste'));
---
+++ resources/js/components/Teste.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Teste() {
+    return (
+        <h1>Teste de integração entre o react e o PHP</h1>
+    )
+}
+
+export default Teste;
